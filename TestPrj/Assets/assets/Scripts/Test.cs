ad1fe7e81a457d8a1a291b61cc8f9a4c097bf019
AssasionOwO <771951340@qq.com>
2017-12-21 14:53:17 +0800

    上传测试文件

---
+++ TestPrj/Assets/assets/Scripts/Test.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if(transform.position==Vector3.zero)
+        {
+            ErrorAbout error = new ErrorAbout();
+            error.ErrorHanding();
+        }
+	}
+}
+
+public class Handle1
+{
+    public void ErrorHanding()
+    {
+        Debug.Log("出现错误，向邮件管理员发送信息！");
+    }
+}
+
+
+public class Handle2
+{
+    public void ErrorHanding()
+    {
+        Debug.Log("出现错误，哔哔哔~~");
+    }
+}
+
+public class Handle3
+{
+    public void ErrorHanding()
+    {
+        Debug.Log("出现错误，哒哒哒~~~~~~~");
+    }
+}
+
+public class ErrorAbout
+{
+    public void ErrorHanding()
+    {
+        Handle1 handle1=new Handle1();
+        Handle2 handle2 = new Handle2();
+        Handle3 handle3 = new Handle3();
+        handle1.ErrorHanding();
+        handle2.ErrorHanding();
+        handle3.ErrorHanding();
+    }
+}
---
+++ TestPrj/TestApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("系统出现错误，需要处理！");
+            ErrorAbout errorAbout = new ErrorAbout();
+            errorAbout.ErrorHanding();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Handle1
+    {
+        public void ErrorHanding()
+        {
+            Console.WriteLine("出现错误！发送了一条信息给管理员！");
+        }
+    }
+
+    public class Handle2
+    {
+        public void ErrorHanding()
+        {
+            Console.WriteLine("发现错误！警报。。。。。。。。。。。。。。");
+        }
+    }
+
+    public class Handle3
+    {
+        public void ErrorHanding()
+        {
+            Console.WriteLine("发现错误！哔哔哔。。。。。。");
+        }
+    }
+
+    public class  ErrorAbout
+    {
+        public void ErrorHanding()
+        {
+            Handle1 handle1=new Handle1();
+            Handle2 handle2=new Handle2();
+            Handle3 handle3=new Handle3();
+
+        }
+    }
+}
