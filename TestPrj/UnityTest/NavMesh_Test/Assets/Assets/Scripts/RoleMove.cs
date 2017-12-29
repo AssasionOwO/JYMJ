5f1eafe4202ef2a0b995ec8da290e4fc6e72921d
AssasionOwO <771951340@qq.com>
2017-12-29 16:24:15 +0800

    测试一堆功能

--- TestPrj/UnityTest/NavMesh_Test/Assets/Assets/Scripts/RoleMove.cs
+++ TestPrj/UnityTest/NavMesh_Test/Assets/Assets/Scripts/RoleMove.cs
@@ -1,55 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 public class RoleMove : MonoBehaviour 
 {
-    private float h;
-    private float v;
-    private Vector3 m_CameraForward;//相机的正前方
-
-    private float m_speed;
-    private float minSpeed=0.0f;//目标的最小移动速度
-    private float maxSpeed=3.0f;//目标的最大移动速度
-
-    public float MaxSpeed
-    {
-        get { return maxSpeed; }
-        set { maxSpeed = value; }
-    }
-
-    public float MinSpeed
-    {
-        get { return minSpeed; }
-        set { minSpeed = value; }
-    }
-
-	// Use this for initialization
-	void Start () 
-    {
-        m_speed = minSpeed;
-	}
+    //private Transform target;//存储需要移动到的目标点
+    //private NavMeshAgent m_angence;//自身的导航寻路组件
+    //void Start () 
+    //{
+    //    target = GameObject.FindGameObjectWithTag("Target").transform;
+    //    m_angence = GetComponent<NavMeshAgent>();
+    //}
 	
-	// Update is called once per frame
-	void Update () 
-    {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-        m_CameraForward = Camera.main.transform.forward;
-        FetchCameraForward();
-        if(h!=0||v!=0)
-        {
-            m_speed = maxSpeed;
-        }
-        else if(h==0&&v==0)
-        {
-            m_speed = minSpeed;
-        }
-        transform.GetComponent<CharacterController>().SimpleMove(new Vector3(h+transform.forward.x, 0, v+transform.forward.z).normalized * m_speed);
-	}
 
-    void FetchCameraForward()
-    {
-        transform.forward = new Vector3(m_CameraForward.x, 0, m_CameraForward.z);
-    }
+    //void Update()
+    //{
+    //    if(Input.GetMouseButtonDown(0))
+    //    {
+    //        Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+    //        RaycastHit hit;
+    //        Physics.Raycast(ray,out hit,Mathf.Infinity);
+    //        if(hit.collider.tag=="Plane")
+    //        {
+    //            target.position = hit.point;
+    //            Debug.Log(target.position);
+    //            m_angence.SetDestination(target.position);
+    //        }
+    //        else
+    //        {
+    //            return;
+    //        }
+    //    }
+    //}
+    private NavMeshAgent navMeshAgent;  
+    private bool is_jumping;  
+  
+    void Start()  
+    {  
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();//初始化navMeshAgent   
+        is_jumping = false;//是否正在跳跃的flag  
+    }  
+  
+    void Update()  
+    {  
+        if (Input.GetMouseButtonDown(0) && !is_jumping)  
+        {//如果不是在跳跃、不是到达自动寻路边缘，按下鼠标左键       
+            //自动寻路    
+            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);  
+            RaycastHit mHit;  
+            if (Physics.Raycast(mRay, out mHit))  
+            {  
+                if (mHit.collider.tag == "Plane")//如果点的是Plane上的一点    
+                {  
+                    navMeshAgent.SetDestination(mHit.point);
+                    Debug.Log(mHit.point);
+                }  
+            }  
+        }  
+       //if (navMeshAgent.isOnOffMeshLink)//如果小球到达跳跃边缘  
+       //{  
+       //    OffMeshLinkData offMeshLinkData = navMeshAgent.currentOffMeshLinkData;  
+       //    transform.LookAt(offMeshLinkData.endPos);//面朝跳跃的结束点  
+       //    navMeshAgent.enabled = false;//关闭自动寻路    
+       //    is_jumping = true;  
+       //    this.GetComponent<Rigidbody>().velocity= new Vector3(offMeshLinkData.endPos.x * 3, 5, offMeshLinkData.endPos.z);  
+       //}  
+       //if (this.transform.position.y < -3)  
+       //{//如果小球跳跃的时候没跳到Plane上面  
+       //    SceneManager.LoadScene("Navmesh_diferent_plane_jump");  
+       //}  
+    }  
+  
+    //如果跳跃之中的小球和Plane开始碰撞    
+   //void OnCollisionEnter(Collision collision)  
+   //{  
+   //    if (collision.collider.gameObject.name == "Plane" && is_jumping)  
+   //    {  
+   //        is_jumping = false;  
+   //        navMeshAgent.enabled = true;//重新打开自动寻路    
+   //    }  
+   //}  
 }
